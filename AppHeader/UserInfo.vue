b0a1860ebdb562b797066417eefa143020376f9b
Bruce Lee <fockerlee@gmail.com>
2018-07-16 10:46:01 +0800

    Add auth for user logoin/logout

--- AppHeader/UserInfo.vue
+++ AppHeader/UserInfo.vue
@@ -1,3 +1,42 @@
 <template>
-  <div/>
+  <div>
+    <p v-if="!isAuthenticated">
+      <nuxt-link :to="path(this, '/auth/login')">Login</nuxt-link>
+    </p>
+    <p v-else>
+      {{ loggedUser ? loggedUser.email : 'Please provide an email.' }} <nuxt-link :to="path(this, '/auth/logout')">Logout</nuxt-link>
+    </p>
+  </div>
 </template>
+
+<script>
+import { show } from '@/plugins/lock'
+import { path } from '@/plugins/utils'
+
+export default {
+  middleware: 'authenticated',
+  computed: {
+    isAuthenticated () {
+      return this.$store.getters.isAuthenticated
+    },
+    loggedUser () {
+      return this.$store.getters.loggedUser
+    },
+  },
+  methods: {
+    path
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import 'assets/css/app';
+@import 'assets/css/colors';
+@import 'assets/css/typography';
+
+.content {
+  max-width: 750px;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
