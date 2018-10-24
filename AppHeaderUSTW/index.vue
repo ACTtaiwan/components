b7431fbcd247a7531b408793d8f0d01ca5a5ca76
hugoinperson <hugoinperson@gmail.com>
2018-10-24 00:43:16 -0500

    change item order

--- AppHeaderUSTW/index.vue
+++ AppHeaderUSTW/index.vue
@@ -34,19 +34,19 @@
               </MenuItem>
             </router-link>
             <router-link
-              :class="{'menu-item-selected': activeMenuItem === 'insights'}"
-              :to="`/insights`"
-              class="menu-item">
-              <MenuItem name="insights">
-              {{ $t('site.menu.insightsItemText') }}
-              </MenuItem>
-            </router-link>
-            <router-link
               :class="{'menu-item-selected': activeMenuItem === 'articles'}"
               :to="`/articles`"
               class="menu-item">
               <MenuItem name="articles">
               {{ $t('site.menu.articlesItemText') }}
+              </MenuItem>
+            </router-link>
+            <router-link
+              :class="{'menu-item-selected': activeMenuItem === 'insights'}"
+              :to="`/insights`"
+              class="menu-item">
+              <MenuItem name="insights">
+              {{ $t('site.menu.insightsItemText') }}
               </MenuItem>
             </router-link>
             <router-link
