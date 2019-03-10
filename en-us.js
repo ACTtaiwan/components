aedd359e6efa7443e63537b8d89437711e349369
Robin Liao <yilia@microsoft.com>
2019-03-10 03:28:53 -0700

    done member landing page

--- en-us.js
+++ en-us.js
@@ -1,8 +1,10 @@
+import enBillsFilters from './BillsFilters/en-us'
 import enUsMembersFilters from './MembersFilters/en-us'
 import enBillSearchResultCard from './BillSearchResultCard/en-us'
 import enSubscription from './Subscription/en-us'
 
 const locale = {
+  ...enBillsFilters,
   ...enUsMembersFilters,
   ...enBillSearchResultCard,
   ...enSubscription
--- zh-tw.js
+++ zh-tw.js
@@ -1,9 +1,13 @@
+import zhTwBillsFilters from './BillsFilters/zh-tw'
 import zhTwMembersFilters from './MembersFilters/zh-tw'
+import zhTwBillSearchResultCard from './BillSearchResultCard/zh-tw'
 import zhTwBillSimpleCard from './BillSimpleCard/zh-tw'
 import zhSubscription from './Subscription/zh-tw'
 
 const locale = {
+  ...zhTwBillsFilters,
   ...zhTwMembersFilters,
+  ...zhTwBillSearchResultCard,
   ...zhTwBillSimpleCard,
   ...zhSubscription
 }
